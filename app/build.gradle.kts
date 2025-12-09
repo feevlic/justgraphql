64d9732c6708ceb492b4d5840781abe7b47f5eae
fevlic <felix@boudnik.de>
2025-12-09 22:55:19 +0100

    update (build.gradle.kts + libs.versions.toml): Add Room and WorkManager Deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,4 +73,11 @@ dependencies {
     ksp(libs.hilt.android.compiler)
     implementation(libs.hilt.android)
     implementation(libs.androidx.hilt.navigation.compose)
+
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+
+    implementation(libs.androidx.work.runtime.ktx)
+    implementation(libs.androidx.hilt.work)
 }
